4beb111268c0f0b33aa17aeeb5766dfd1cf49188
Iason Bakas <bakasiason@gmail.com>
2018-11-20 22:12:22 +0200

    First Steps

--- foodshare/fs/views.py
+++ foodshare/fs/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render, get_object_or_404
 
+from django.urls import reverse
+from django.utils import timezone
+from django.http.response import HttpResponseRedirect
+
 from .models import Post, User, Rating, Donation, Product
 
 def index(request):
@@ -14,3 +18,7 @@ def post(request, post_id):
 def user(request, user_id):
 	user = get_object_or_404(User, pk=user_id)
 	return render(request, 'fs/user.html', {'user': user})
+
+def product(request, product_id):
+	product = get_object_or_404(Product, pk=product_id)
+	return render(request, 'fs/product.html', {'product': product})
